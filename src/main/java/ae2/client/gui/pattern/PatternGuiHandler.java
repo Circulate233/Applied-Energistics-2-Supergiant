77f1dfcababc0024706eee73553d262bc4123e3a
xinyihl <1012737146@qq.com>
2026-08-05 15:49:48 +0800

    添加API用于注册样板预览GUI

--- src/main/java/ae2/client/gui/pattern/PatternGuiHandler.java
+++ src/main/java/ae2/client/gui/pattern/PatternGuiHandler.java
@@ -7,14 +7,34 @@ import ae2.container.pattern.ContainerProcessingPattern;
 import ae2.crafting.pattern.AECraftingPattern;
 import ae2.crafting.pattern.AEProcessingPattern;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SideOnly(Side.CLIENT)
 public final class PatternGuiHandler {
 
-    private PatternGuiHandler() {
+    private static final Map<Class<?> ,GuiScreenGetter> PATTERN_GUI_MAP = new HashMap<>();
+
+    static {
+        register(AECraftingPattern.class, ( pattern)->{
+            Minecraft minecraft = Minecraft.getMinecraft();
+            ContainerCraftingPattern container = new ContainerCraftingPattern(minecraft.player.inventory, pattern);
+            return new GuiCraftingPattern(container, minecraft.player.inventory);
+        });
+        register(AEProcessingPattern.class, (pattern)->{
+            Minecraft minecraft = Minecraft.getMinecraft();
+            ContainerProcessingPattern container = new ContainerProcessingPattern(minecraft.player.inventory, pattern);
+            return new GuiProcessingPattern(container, minecraft.player.inventory);
+        });
+    }
+
+    public static void register(Class<?> clazz, GuiScreenGetter getter) {
+        PATTERN_GUI_MAP.put(clazz, getter);
     }
 
     public static boolean open(ItemStack pattern) {
@@ -24,16 +44,23 @@ public final class PatternGuiHandler {
         }
 
         IPatternDetails details = PatternDetailsHelper.decodePattern(pattern, minecraft.world);
-        if (details instanceof AECraftingPattern) {
-            ContainerCraftingPattern container = new ContainerCraftingPattern(minecraft.player.inventory, pattern);
-            minecraft.displayGuiScreen(new GuiCraftingPattern(container, minecraft.player.inventory));
-            return true;
+
+        if (details == null) {
+            return false;
         }
-        if (details instanceof AEProcessingPattern) {
-            ContainerProcessingPattern container = new ContainerProcessingPattern(minecraft.player.inventory, pattern);
-            minecraft.displayGuiScreen(new GuiProcessingPattern(container, minecraft.player.inventory));
-            return true;
+
+        GuiScreenGetter getter = PATTERN_GUI_MAP.get(details.getClass());
+
+        if (getter == null) {
+            return false;
         }
-        return false;
+
+        minecraft.displayGuiScreen(getter.getGuiScreen(pattern));
+        return true;
+    }
+
+    @FunctionalInterface
+    public interface GuiScreenGetter {
+        GuiScreen getGuiScreen(ItemStack pattern);
     }
 }
