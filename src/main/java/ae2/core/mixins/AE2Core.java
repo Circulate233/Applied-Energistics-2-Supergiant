605ed784209ec6ae769f6e6f9fa645d42b84f063
vcwdfca <2168409010@qq.com>
2026-08-31 16:11:58 +0800

    remove deprecated IEarlyMixinLoader

--- src/main/java/ae2/core/mixins/AE2Core.java
+++ src/main/java/ae2/core/mixins/AE2Core.java
@@ -3,17 +3,13 @@ package ae2.core.mixins;
 import io.netty.util.internal.EmptyArrays;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import org.jetbrains.annotations.Nullable;
-import zone.rong.mixinbooter.IEarlyMixinLoader;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
-@SuppressWarnings("deprecation")
 @IFMLLoadingPlugin.Name("AE2Core")
 @IFMLLoadingPlugin.SortingIndex(-1)
 @IFMLLoadingPlugin.MCVersion("1.12.2")
-public class AE2Core implements IFMLLoadingPlugin, IEarlyMixinLoader {
+public class AE2Core implements IFMLLoadingPlugin {
 
     @Nullable
     public String[] getASMTransformerClass() {
@@ -38,8 +34,4 @@ public class AE2Core implements IFMLLoadingPlugin, IEarlyMixinLoader {
         return null;
     }
 
-    @Override
-    public List<String> getMixinConfigs() {
-        return Collections.singletonList("ae2.default.mixins.json");
-    }
 }
